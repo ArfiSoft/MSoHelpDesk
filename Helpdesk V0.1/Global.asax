ff54f28ee073891f16301cddae991c83027fa00e
Arvid Hamersma <a.hamersma@viadata.nl>
2015-07-23 15:40:38 +0200

    New project

---
+++ Helpdesk V0.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Helpdesk_V0._1.MvcApplication" Language="C#" %>
---
+++ Helpdesk V0.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Helpdesk_V0._1.Startup))]
+namespace Helpdesk_V0._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
